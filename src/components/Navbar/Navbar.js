afbe43fcca50448de5da8443113c2203e42524af
kate <kan004@ucr.edu>
2020-10-02 22:58:07 -0700

    added navbar component and css

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    }
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        LOGO <i className="fas fa-bug"></i>
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    </div>
+                    
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                            <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+                                Projects
+                            </Link>
+                            <Link to='/contact' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>CONTACT</Button>}
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
