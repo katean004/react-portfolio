3f55a32497f4d14e3c28b5a11790d40876d56f40
kate <kan004@ucr.edu>
2020-10-03 18:09:23 -0700

    Updates

---
+++ precache-manifest.afbf07bd927800a7544c2972cefa9f39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f3687332f5452a424a4965f7fb3051",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a1dd72c23abb58378577",
+    "url": "/react-portfolio/static/css/main.5d77ef18.chunk.css"
+  },
+  {
+    "revision": "58cc7ecaef4c9f8c3580",
+    "url": "/react-portfolio/static/js/2.91a0262d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.91a0262d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1dd72c23abb58378577",
+    "url": "/react-portfolio/static/js/main.398b28ff.chunk.js"
+  },
+  {
+    "revision": "4d38b31ab485b933a266",
+    "url": "/react-portfolio/static/js/runtime-main.56b30d05.js"
+  }
+]);
