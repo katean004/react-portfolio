41b48edd6292f7568901de7e6aca86d6cdb7aa88
kate <kan004@ucr.edu>
2020-10-05 18:40:15 -0700

    took out button code navbar js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,20 +5,20 @@ import './Navbar.css'
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+    // const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if(window.innerWidth <= 960){
-            setButton(false);
-        }else{
-            setButton(true);
-        }
-    }
+    // const showButton = () => {
+    //     if(window.innerWidth <= 960){
+    //         setButton(false);
+    //     }else{
+    //         setButton(true);
+    //     }
+    // }
 
-    window.addEventListener('resize', showButton);
+    // window.addEventListener('resize', showButton);
 
     return (
         <>
